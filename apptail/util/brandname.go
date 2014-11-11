321deb193504b0280f8f1ff3e7dffb69300ca872
Sridhar Ratnakumar <github@srid.name>
2014-11-10 18:46:35 -0800

    load cc_ng info/name as brandname

--- apptail/util/brandname.go
+++ apptail/util/brandname.go
@@ -1,6 +1,34 @@
 package util
 
+import (
+	"github.com/ActiveState/logyard-apps/common"
+	"github.com/ActiveState/stackato-go/server"
+)
+
+type Config struct {
+	Info struct {
+		Name string `json:"name"`
+	} `json:"info"`
+}
+
+var c *server.Config
+
+func getConfig() *Config {
+	return c.GetConfig().(*Config)
+}
+
+func loadConfig() {
+	var err error
+	c, err = server.NewConfig("apptail", Config{})
+	if err != nil {
+		common.Fatal("Unable to load apptail config; %v", err)
+	}
+}
+
 func GetBrandName() string {
-	// TODO: read from cc_ng config
-	return "stackato"
+	return getConfig().Info.Name
+}
+
+func init() {
+	loadConfig()
 }
